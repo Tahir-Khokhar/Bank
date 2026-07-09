99cafedbb5cd9054401786c046a81c33e3e17958
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-09 17:57:35 +0500

    Update employee URLs to include 'employees/' prefix

--- employees/urls.py
+++ employees/urls.py
@@ -4,7 +4,7 @@ from .views import EmployeeLoginView, EmployeeRegisterView
 
 
 urlpatterns = [
-    path('login/', EmployeeLoginView.as_view(), name='employee-login'),
-    path('register/', EmployeeRegisterView.as_view(), name='employee-register'),
+    path('employees/login/', EmployeeLoginView.as_view(), name='employee-login'),
+    path('employees/register/', EmployeeRegisterView.as_view(), name='employee-register'),
 ]
 
